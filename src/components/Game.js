e338dc10b8d0ec0c1a77da4c908b12e8e0aae900
George Tsiou <georgtsiou@gmail.com>
2020-08-15 20:15:13 +0300

    cleanup, passed funcs to children instead of calling them in Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,81 +1,86 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./../styles/Game.css";
 import Board from "./BoardPanel/Board";
 import calculateWinner from "./../utils/calculateWinner";
 import StatusPanel from "./StatusPanel/StatusPanel";
-import currentStatusText from './../utils/currentStatusText';
-
+import currentStatusText from "./../utils/currentStatusText";
 
 const Game = () => {
-    const [stepNumber, setStepNumber] = useState(0);
-    const [xIsNext, setNext] = useState(true);
-    const [history, setHistory] = useState([{
-        squares: Array(9).fill(null),
-        currentMove: null
-    }]);
-    const [hovered, setHovered] = useState(null);
+	const [stepNumber, setStepNumber] = useState(0);
+	const [xIsNext, setNext] = useState(true);
+	const [history, setHistory] = useState([
+		{
+			squares: Array(9).fill(null),
+			currentMove: null,
+		},
+	]);
+	const [hovered, setHovered] = useState(null);
 
-    const handleClick = markedTile => {
-        const currHistory = history.slice(0, stepNumber + 1);        
-        const current = currHistory[currHistory.length -1];
-        const squares = current.squares.slice();
-        if(squares[markedTile]){
-            return;
-        }
+	const handleClick = (markedTile) => {
+		const currHistory = history.slice(0, stepNumber + 1);
+		const current = currHistory[currHistory.length - 1];
+		const squares = current.squares.slice();
+		if (squares[markedTile]) {
+			return;
+		}
 
-        const {draw, winner} = calculateWinner(squares);
-        if(winner || draw ) {
-            return;
-        }
+		const { draw, winner } = calculateWinner(squares);
+		if (winner || draw) {
+			return;
+		}
 
-        squares[markedTile] = xIsNext ? "X" : "O";
-        setHistory([
-            ...currHistory,
-                {
-                    squares: squares,
-                    currentMove: markedTile
-                }
-            ]);
-        setStepNumber(currHistory.length);
-        setNext(!xIsNext);
-    }
+		squares[markedTile] = xIsNext ? "X" : "O";
+		setHistory([
+			...currHistory,
+			{
+				squares: squares,
+				currentMove: markedTile,
+			},
+		]);
+		setStepNumber(currHistory.length);
+		setNext(!xIsNext);
+	};
 
-    const jumpTo = step => {
-        setStepNumber(step);
-        setNext((step % 2) === 0);
-    }
+	const jumpTo = (step) => {
+		setStepNumber(step);
+		setNext(step % 2 === 0);
+	};
 
-    const handleMouseOver = tileNumber => {
-        setHovered(tileNumber);
-    }
+	const handleMouseOver = (tileNumber) => {
+		setHovered(tileNumber);
+	};
 
-    const handleMouseOut = () => {
-        setHovered(null);
-    }
+	const handleMouseOut = () => {
+		setHovered(null);
+	};
 
-    const current = history[stepNumber];
-    const { winningLine, draw, winner } = calculateWinner(current.squares);
-    const currentStatus = currentStatusText(winner, draw, current.squares[winningLine[0]], xIsNext)
-    const moves = history.map(({currentMove}, move) => ({currentMove, move}));
+	const current = history[stepNumber];
+	const { winningLine, draw, winner } = calculateWinner(current.squares);
+	const currentStatus = currentStatusText(
+		winner,
+		draw,
+		current.squares[winningLine[0]],
+		xIsNext
+	);
+	const moves = history.map(({ currentMove }, move) => ({ currentMove, move }));
 
-
-    return(
-        <div className="game">
-            <Board
-                winningLine={winningLine}
-                squares={current.squares}
-                onClick={i => handleClick(i)}
-                onMouseOver={i => handleMouseOver(i)}
-                onMouseOut={() => handleMouseOut()}
-            />
-            <StatusPanel
-                status={currentStatus}
-                moves={moves}
-                hovered={hovered}
-                onClick={step => jumpTo(step)}
-            />
-        </div>
-    );
-}
+	return (
+		<div className="game">
+			<Board
+				winningLine={winningLine}
+				squares={current.squares}
+				onClick={handleClick}
+				onMouseOver={handleMouseOver}
+				onMouseOut={handleMouseOut}
+			/>
+			<StatusPanel
+				status={currentStatus}
+				moves={moves}
+				hovered={hovered}
+				onClick={jumpTo}
+			/>
+		</div>
+	);
+};
 
 export default Game;
