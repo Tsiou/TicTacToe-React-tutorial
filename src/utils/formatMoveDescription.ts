454a1a7b6895f54f2ae7aa1b4903693205e3b830
George Tsiou <Georgtsiou@gmail.com>
2022-02-13 16:49:05 +0200

    fixed math operation priority bug

--- src/utils/formatMoveDescription.ts
+++ src/utils/formatMoveDescription.ts
@@ -1,6 +1,6 @@
 const formatMoveDescription = (move: number, currentMove: number | null) => {
-    const row = Math.floor(currentMove?? 0 / 3) + 1;
-    const col = currentMove ?? 0 % 3 + 1;
+    const row = Math.floor( (currentMove?? 0) / 3) + 1;
+    const col = (currentMove ?? 0) % 3 + 1;
 
     return move ?
         `Go to move #${move} (${row}, ${col})` :
