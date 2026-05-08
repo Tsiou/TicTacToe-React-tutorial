d579250986bddab2f83cdc7f0b04f24552e86f5b
George Tsiou <Georgtsiou@gmail.com>
2026-05-08 11:50:05 +0300

    moved to vite toolchain, updated dependencies

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import Game from "./components/Game";
+import "./../src/styles/style.css";
+import { StrictMode } from "react";
+
+const container = document.getElementById("root");
+if (container) {
+	const root = createRoot(container);
+	root.render(
+		<StrictMode>
+			<Game />
+		</StrictMode>,
+	);
+}
